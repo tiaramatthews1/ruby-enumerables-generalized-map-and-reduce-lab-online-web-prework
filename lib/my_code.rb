6d37087ee08060b8f583977ca7b524f295905563
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-08 03:36:06 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,11 +1,11 @@
 def map(array)
-  new_array = []
+  new = []
   i = 0
   while i < array.length do
     new.push(yield ( array[i]) ) 
     i += 1
   end
- new_array
+ new
 end
 
 def reduce(array, start = nil)
